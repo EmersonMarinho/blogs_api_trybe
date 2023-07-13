1cdd288feb2fbb6e7ff0900f5c052ff9ffa146fe
EmersonMarinho <Emerson_marinhoaz@hotmail.com>
2023-07-13 16:14:34 -0300

    feat requisito 02

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            allowNull:false,
+            autoIncrement:true,
+            primaryKey:true,
+            type:DataTypes.INTEGER
+        },
+        displayName: {
+            allowNull:false,
+            type:DataTypes.STRING
+        },
+        email: {
+            allowNull:false,
+            type:DataTypes.STRING
+        },
+        password: {
+            allowNull:false,
+            type:DataTypes.STRING
+        },
+        image: {
+            type: DataTypes.STRING,
+        },
+        }, {
+            timestamps: false,
+            tableName: 'users',
+            underscored: true,
+        }
+    );
+
+    return User;
+};
