73de76f9adbed77f7076f96b234297718c185672
EmersonMarinho <Emerson_marinhoaz@hotmail.com>
2023-08-07 16:34:12 -0300

    feat: req 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+    const PostCategory = sequelize.define('PostCategory', {
+       postId: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         primaryKey: true, 
+       },
+       categoryId: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         primaryKey: true,
+       },
+     }, {
+       underscored: true,
+       timestamps: false,
+       tableName: 'posts_categories',
+     });
+ 
+     PostCategory.associate = ({ BlogPost, Category }) => {
+       Category.belongsToMany(BlogPost, {
+         through: PostCategory,
+         foreignKey: 'postId',
+         otherKey: 'categoryId',
+       });
+ 
+       BlogPost.belongsToMany(Category, {
+         through: PostCategory,
+         foreignKey: 'categoryId',
+         otherKey: 'postId'
+       });
+     };
+ 
+     return PostCategory;
+   };
